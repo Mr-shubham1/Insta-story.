8f17e87213c7a90956575fd00983ddeb3ffce1d6
Mr-shubham1 <108973912+Mr-shubham1@users.noreply.github.com>
2024-03-03 14:50:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+function storiesaddkaroaurdekho(){
+
+    var stories = [
+        {dp:"https://i.pinimg.com/236x/b2/09/53/b20953d2af68ba2cb7bda7252a3bb960.jpg",
+        status:"https://i.pinimg.com/236x/b2/09/53/b20953d2af68ba2cb7bda7252a3bb960.jpg"},
+        {dp:"https://i.pinimg.com/originals/c9/8c/ac/c98cacb6f83ec413bec40bfb8fcb3574.jpg",
+        status:"https://i.pinimg.com/originals/c9/8c/ac/c98cacb6f83ec413bec40bfb8fcb3574.jpg"},
+        {dp:"https://image.cdn2.seaart.ai/2023-10-05/19067012879757317/82bf9f46e0e16a9f8c51624bfb0b169d6c647cfe_high.webp",
+        status:"https://image.cdn2.seaart.ai/2023-10-05/19067012879757317/82bf9f46e0e16a9f8c51624bfb0b169d6c647cfe_high.webp"},
+        {dp:"https://cdn.gencraft.com/prod/user/82aab44f-50df-4e0d-8463-a3f672cab6ac/f197b189-2c64-4ac0-bd7e-22c28e6a1f29/image/image1_0.jpg?Expires=1709536671&Signature=DuVjiEpg3h1jnPdVOrH6IjYmG7jy05Z5e1vDPfjYkMVA0Qk6HozlbLk5L3inQaMjDggU9BNnVLI0TYOL9Lm6MkBCk5xirsAtLGX6VihXnKAkO5pSv~LOLbmX18s4~se0RWHwHwbzbhWDhzCyIVrkxLEtcmsvOXucMLPThlOxwLQjNYl36mX6B8zBWiyN5PqBd38YQTizEQdSK3mfMgXiqHlRjFRbac1eN40DImAeFmud~XGlDNO-E3PKSm6cUV2bQ3e06vRjcGKwNmImktqnNZJ-w7vizLxH0CDEBkcQ0lHAu6y3pXZdsLfN1aBUpPIrFAwkCeFkr2ExbDxlSrGUIA__&Key-Pair-Id=K3RDDB1TZ8BHT8",
+        status:"https://cdn.gencraft.com/prod/user/82aab44f-50df-4e0d-8463-a3f672cab6ac/f197b189-2c64-4ac0-bd7e-22c28e6a1f29/image/image1_0.jpg?Expires=1709536671&Signature=DuVjiEpg3h1jnPdVOrH6IjYmG7jy05Z5e1vDPfjYkMVA0Qk6HozlbLk5L3inQaMjDggU9BNnVLI0TYOL9Lm6MkBCk5xirsAtLGX6VihXnKAkO5pSv~LOLbmX18s4~se0RWHwHwbzbhWDhzCyIVrkxLEtcmsvOXucMLPThlOxwLQjNYl36mX6B8zBWiyN5PqBd38YQTizEQdSK3mfMgXiqHlRjFRbac1eN40DImAeFmud~XGlDNO-E3PKSm6cUV2bQ3e06vRjcGKwNmImktqnNZJ-w7vizLxH0CDEBkcQ0lHAu6y3pXZdsLfN1aBUpPIrFAwkCeFkr2ExbDxlSrGUIA__&Key-Pair-Id=K3RDDB1TZ8BHT8"},
+        {dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkqtUUa7Lcm4CzqUi9jd6yl_3kJz0jwTTLTM_dhjjHuV5kQE35gJE2BsK72suzwTzykFQ&usqp=CAU",
+        status:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkqtUUa7Lcm4CzqUi9jd6yl_3kJz0jwTTLTM_dhjjHuV5kQE35gJE2BsK72suzwTzykFQ&usqp=CAU"},
+        {dp:"https://image.cdn2.seaart.ai/2023-08-08/53998625370181/52f12b858029674a89f50772535a931e9973f273_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-08-08/53998625370181/52f12b858029674a89f50772535a931e9973f273_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-11-06/21921317217380357/400f2fa7c6269e9ea2448b864bc1910931fe0242_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-11-06/21921317217380357/400f2fa7c6269e9ea2448b864bc1910931fe0242_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-10-27/21020199735046149/5d3fdb6ef5ebbeedd87fa8d4a3c88e7ad876fc2c_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-10-27/21020199735046149/5d3fdb6ef5ebbeedd87fa8d4a3c88e7ad876fc2c_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-12-27/cm63n85e878c73cqil40/7e8ada8e01244146ad44b7343cfec503d68bab38_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-12-27/cm63n85e878c73cqil40/7e8ada8e01244146ad44b7343cfec503d68bab38_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-12-21/cm229i5e878c73apc3kg/40844c983a56a8bc2b82022d301a234ac35da424_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-12-21/cm229i5e878c73apc3kg/40844c983a56a8bc2b82022d301a234ac35da424_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-12-23/cm3it1te878c73b6v4vg/98db82f99fc2369a72f84b4b3928a84bda9e4a13_high.webp",
+        status:"https://image.cdn2.seaart.ai/2023-12-23/cm3it1te878c73b6v4vg/98db82f99fc2369a72f84b4b3928a84bda9e4a13_high.webp"},
+        {dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDAtwZSFqAoJUpddtwYN2D_8PBgP0WFhX7pqCRcQXkPyGryBZ8eg6Rxa9r-NbJ5XznXTc&usqp=CAU",
+        status:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDAtwZSFqAoJUpddtwYN2D_8PBgP0WFhX7pqCRcQXkPyGryBZ8eg6Rxa9r-NbJ5XznXTc&usqp=CAU"},
+        {dp:"https://image.cdn2.seaart.ai/2023-08-28/15555772261883909/1b3ae435ce1aaf6451082aec7140e8d00bf95835_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-08-28/15555772261883909/1b3ae435ce1aaf6451082aec7140e8d00bf95835_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-08-28/15555185085538309/bd10bad3bce831ac755d61aeedc309f1c9c7ce09_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-08-28/15555185085538309/bd10bad3bce831ac755d61aeedc309f1c9c7ce09_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-08-24/15275065174155269/4ad7b7e0ae67bab9c421c0214eb39ae8c49ea62d_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-08-24/15275065174155269/4ad7b7e0ae67bab9c421c0214eb39ae8c49ea62d_low.webp"},
+        {dp:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqIogKSU_Zo_5CdH7heUgzKbysmNn3YhN5ziGsub8JFzCpnRE1JrQu1ihH4-SYR68NKB8&usqp=CAU",
+        status:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqIogKSU_Zo_5CdH7heUgzKbysmNn3YhN5ziGsub8JFzCpnRE1JrQu1ihH4-SYR68NKB8&usqp=CAU"},
+        {dp:"https://image.cdn2.seaart.ai/2023-09-05/16320779144707077/e59751f104a2525b790b2b2fa11ea11339b234be_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-09-05/16320779144707077/e59751f104a2525b790b2b2fa11ea11339b234be_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-09-14/17126675551655941/d432d337e264d416eff76775ece7dd206dc34e66_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-09-14/17126675551655941/d432d337e264d416eff76775ece7dd206dc34e66_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-08-04/52593517350981/a534bfa45679be19d750ffb5b34591b42b9a3f49_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-08-04/52593517350981/a534bfa45679be19d750ffb5b34591b42b9a3f49_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-12-05/clniq5p4msbc73c8577g/b4cf0b63815eb5a32cd821fc32c2efdef99cc14b_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-12-05/clniq5p4msbc73c8577g/b4cf0b63815eb5a32cd821fc32c2efdef99cc14b_low.webp"},
+        {dp:"https://image.cdn2.seaart.ai/2023-10-19/20315731853845509/671520f4cd8fdcc111cd2ddea95ec3d925ba8b7c_low.webp",
+        status:"https://image.cdn2.seaart.ai/2023-10-19/20315731853845509/671520f4cd8fdcc111cd2ddea95ec3d925ba8b7c_low.webp"}
+    ];
+
+    var storiyan = document.querySelector("#storiyan");
+
+    stories.forEach(function(story,index){
+        storiyan.innerHTML += `<div class="stories">
+        <img id = "${index}" src="${story.dp}" alt="">
+        </div>`;
+    })
+
+    storiyan.addEventListener("click",function(dets){
+        // console.log(dets.target.id);
+        // console.log(stories[dets.target.id].status);
+        document.querySelector("#cards .viewstory").innerHTML = `<img class="storyimage" src="${stories[dets.target.id].status}" alt=""> <div class="bar"></div>`;
+        document.querySelector("#cards .viewstory").style.display = "inline";
+        bartimer();
+
+        setTimeout(function(){
+            document.querySelector("#cards .viewstory").style.display = "none";
+        },3000)
+    })
+
+    
+
+
+}
+
+
+
+
+storiesaddkaroaurdekho();
+
+function bartimer(){
+    gsap.to(".bar",{
+        width:"0px",
+        duration : 3,
+    })
+}
+
+
